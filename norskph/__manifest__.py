d9450a6655cca4a52cda5e3a2c1f516f619d8df7
Henrik Norlin <henrik.norl@gmail.com>
2025-08-04 18:23:22 +0200

    [ADD] norskph

---
+++ norskph/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2025 Loym
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "norskph",
+    "summary": "",
+    "author": "Loym",
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "depends": [
+        "documents",
+        "mail_tracking",
+        "project",
+        "project_partner_rel",
+        
+    ],
+    "license": "AGPL-3",
+    "version": "18.0.1.0.0",
+    "website": "https://www.loym.com",
+}
