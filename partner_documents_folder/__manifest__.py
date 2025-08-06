4ef429c81eaa4f7048b955018d5d237daa1c48b8
Henrik Norlin <henrik.norl@gmail.com>
2025-08-06 11:08:24 +0200

    [ADD] partner_documents_folder

---
+++ partner_documents_folder/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2025 Loym
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Contact Documents Folder",
+    "summary": "",
+    "author": "Loym, FIQ",
+    "data": [
+        "data/documents_document_data.xml",
+        "views/res_partner_views.xml",
+    ],
+    "depends": [
+        "base",
+        "documents",
+    ],
+    "license": "AGPL-3",
+    "version": "18.0.1.0.0",
+    "website": "https://www.loym.com",
+}
---
+++ partner_documents_folder/models/res_partner.py
@@ -0,0 +1,73 @@
+from odoo import api, fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    partner_folder_id = fields.Many2one(
+        "documents.document",
+        string="Contact Folder",
+    )
+    customer_folder_id = fields.Many2one(
+        "documents.document",
+        string="Customer Folder",
+    )
+    supplier_folder_id = fields.Many2one(
+        "documents.document",
+        string="Supplier Folder",
+    )
+
+    partner_folder_url = fields.Char(
+        string="Contact Folder URL",
+        related="partner_folder_id.url",
+        readonly=False,
+    )
+    customer_folder_url = fields.Char(
+        string="Customer Folder URL",
+        related="customer_folder_id.url",
+        readonly=False,
+    )
+    supplier_folder_url = fields.Char(
+        string="Supplier Folder URL",
+        related="supplier_folder_id.url",
+        readonly=False,
+    )
+
+    @api.constrains("partner_folder_url")
+    def _constrains_partner_folder_url(self):
+        if self.partner_folder_url and not self.partner_folder_id:
+            root = "partner_documents_folder.documents_document_partner_folder"
+            self.partner_folder_id = self.env["documents.document"].create(
+                {
+                    "name": self.name,
+                    "url": self.partner_folder_url,
+                    "type": "folder",
+                    "folder_id": self.env.ref(root).id,
+                }
+            )
+
+    @api.constrains("customer_folder_url")
+    def _constrains_customer_folder_url(self):
+        if self.customer_folder_url and not self.customer_folder_id:
+            root = "partner_documents_folder.documents_document_customer_folder"
+            self.customer_folder_id = self.env["documents.document"].create(
+                {
+                    "name": self.name,
+                    "url": self.customer_folder_url,
+                    "type": "folder",
+                    "folder_id": self.env.ref(root).id,
+                }
+            )
+
+    @api.constrains("supplier_folder_url")
+    def _constrains_supplier_folder_url(self):
+        if self.supplier_folder_url and not self.supplier_folder_id:
+            root = "partner_documents_folder.documents_document_supplier_folder"
+            self.supplier_folder_id = self.env["documents.document"].create(
+                {
+                    "name": self.name,
+                    "url": self.supplier_folder_url,
+                    "type": "folder",
+                    "folder_id": self.env.ref(root).id,
+                }
+            )
