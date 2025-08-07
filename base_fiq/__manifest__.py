f777c4384da3c9cfaacb9f61231ccbe63994db00
Henrik Norlin <henrik.norl@gmail.com>
2025-08-07 09:27:53 +0200

    base_fiq: Apps: show technical data

--- base_fiq/__manifest__.py
+++ base_fiq/__manifest__.py
@@ -5,8 +5,11 @@
     "summary": "",
     "author": "FIQ as, Loym AS, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/knowledge",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "license": "AGPL-3",
+    "data": [
+        "views/ir_module_views.xml",
+    ],
     "depends": [
         "base_unique_code",
         "document_url", # OCA/document (attachment url)
