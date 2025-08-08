55e7933063116dadf5a134934f31d5c8bfeb995b
Henrik Norlin <henrik.norl@gmail.com>
2025-08-08 14:08:20 +0200

    partner_documents_folder: form view

--- partner_documents_folder/__manifest__.py
+++ partner_documents_folder/__manifest__.py
@@ -14,7 +14,7 @@
         "documents",
     ],
     "license": "AGPL-3",
-    "version": "18.0.1.0.3",
+    "version": "18.0.1.0.4",
     "website": "https://www.loym.com",
 }
 # norskph: deaktivert sikkerhetsregler "Post-regel"
