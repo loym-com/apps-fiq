7c5babfce14ee9e4739531debe0383f9714b19cd
Henrik Norlin <henrik.norl@gmail.com>
2025-08-08 10:19:03 +0200

    documents_name: Just one function, run it after install

--- documents_name/__manifest__.py
+++ documents_name/__manifest__.py
@@ -5,10 +5,11 @@
     "summary": "",
     "author": "FIQ as, Loym AS, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/knowledge",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "license": "AGPL-3",
     "depends": ["documents"],
     "data": [
         "views/documents_document_views.xml",
     ],
+    "post_init_hook": "post_init_hook",
 }
--- documents_name/models/documents_document.py
+++ documents_name/models/documents_document.py
@@ -8,17 +8,11 @@ class DocumentsDocument(models.Model):
         translate=True,
     )
 
-    @api.onchange("name_translate")
-    def _onchange_name_translate(self):
-        if self.name_translate:
-            self.name = self.name_translate
-
-    @api.constrains("name_translate")
-    def _constrains_name_translate(self):
+    @api.onchange("name", "name_translate")
+    @api.constrains("name", "name_translate")
+    def _set_name_or_name_translate(self):
         for record in self:
-            record.name = record.name_translate
-
-    @api.constrains("name")
-    def _constrains_name_translate(self):
-        for record in self:
-            record.name_translate = record.name
+            if record.name_translate:
+                record.name = record.name_translate
+            elif record.name:
+                record.name_translate = record.name
