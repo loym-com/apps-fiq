04c45882f98261277f9c5d5bd964b79ec2ed8cb4
Henrik Norlin <henrik.norl@gmail.com>
2025-08-11 21:34:20 +0200

    documents_color: disabled non-working javascript

--- documents_color/static/src/color_picker.js
+++ documents_color/static/src/color_picker.js
@@ -1,30 +1,31 @@
-// Code suggestion from Odoo AI
+// // Code suggestion from Odoo AI
+// // IT DOES NOT WORK AND IT CREATES ERRORS!!!
 
-odoo.define('your_module.kanban_color_picker', function (require) {
-    "use strict";
+// odoo.define('your_module.kanban_color_picker', function (require) {
+//     "use strict";
 
-    var KanbanRenderer = require('web.KanbanRenderer');
-    var rpc = require('web.rpc');
+//     var KanbanRenderer = require('web.KanbanRenderer');
+//     var rpc = require('web.rpc');
 
-    KanbanRenderer.include({
-        events: _.extend({}, KanbanRenderer.prototype.events, {
-            'change .o_color_picker': '_onColorChange',
-        }),
+//     KanbanRenderer.include({
+//         events: _.extend({}, KanbanRenderer.prototype.events, {
+//             'change .o_color_picker': '_onColorChange',
+//         }),
 
-        _onColorChange: function (event) {
-            var self = this;
-            var color = $(event.target).val(); // Get the selected color value
-            var recordId = $(event.target).data('record-id'); // Get the ID of the record
+//         _onColorChange: function (event) {
+//             var self = this;
+//             var color = $(event.target).val(); // Get the selected color value
+//             var recordId = $(event.target).data('record-id'); // Get the ID of the record
 
-            rpc.query({
-                model: 'documents.document',
-                method: 'write',
-                args: [[recordId], {'color': color}],
-            }).then(function () {
-                // Optionally, you can add some visual feedback here,
-                // like re-rendering the Kanban card.
-                self.updateRecord(recordId);
-            });
-        },
-    });
-});
+//             rpc.query({
+//                 model: 'documents.document',
+//                 method: 'write',
+//                 args: [[recordId], {'color': color}],
+//             }).then(function () {
+//                 // Optionally, you can add some visual feedback here,
+//                 // like re-rendering the Kanban card.
+//                 self.updateRecord(recordId);
+//             });
+//         },
+//     });
+// });
