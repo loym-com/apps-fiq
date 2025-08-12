f682082152b509014e4a3dd5b3e6044895c9e52a
Henrik Norlin <henrik.norl@gmail.com>
2025-08-12 14:59:13 +0200

    project_documents_tab: unique field names

--- project_documents_tab/models/project_project.py
+++ project_documents_tab/models/project_project.py
@@ -4,11 +4,10 @@ from odoo import api, fields, models
 class ProjectProject(models.Model):
     _inherit = "project.project"
 
-    use_documents = fields.Boolean("Use Documents") # Override documents_project
     document_ids = fields.Many2many(
         "documents.document",
         compute="_compute_document_ids",
-        string="Documents",
+        # string="Documents", # use_documents in documents_project and documents_fsm
     )
 
     def _compute_document_ids(self):
