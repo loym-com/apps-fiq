0139f74194d23dd34d8c871bd1f452210d7ab77b
Henrik Norlin <henrik.norl@gmail.com>
2025-08-26 08:18:07 +0200

    portal_user: dummy users

--- portal_user/__manifest__.py
+++ portal_user/__manifest__.py
@@ -6,11 +6,12 @@
     "summary": "",
     "author": "FIQ, Loym",
     "data": [
+        "data/res_users_data.xml",
         "wizard/portal_wizard_views.xml",
     ],
     "depends": [
         "portal",
     ],
     "license": "AGPL-3",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
 }
