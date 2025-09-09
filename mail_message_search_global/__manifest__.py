a42d1257bc0e995368eca9f24a3d90a441e49862
Henrik Norlin <henrik.norl@gmail.com>
2025-09-09 22:20:27 +0200

    mail_message_search_global

---
+++ mail_message_search_global/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Loym AS
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Mail Message Search Global",
+    "summary": "",
+    "author": "FIQ, Loym, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/mail",
+    "category": "",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["mail_message_search"],
+    "data": [
+        "views/mail_message_views.xml",
+    ],
+}
