827baf50bcc7aad823762e9ca9ed3e949da11103
Henrik Norlin <henrik.norl@gmail.com>
2025-10-16 22:12:47 +0200

    dummy18a

---
+++ dummy18a/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2025 FIQ & Loym
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+
+{
+    "name": "dummy18a",
+    "summary": "",
+    "author": "FIQ, Loym",
+    "data": [
+    ],
+    "depends": [
+        "base",
+    ],
+    "license": "AGPL-3",
+    "version": "18.0.1.0.0",
+}
