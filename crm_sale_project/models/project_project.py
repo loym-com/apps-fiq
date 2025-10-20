d489d2a8b6adb2ae6dd48c46521163d776f6620a
Henrik Norlin <henrik.norl@gmail.com>
2025-10-20 21:54:20 +0200

    dummy partial delete

--- crm_sale_project/models/project_project.py
+++ crm_sale_project/models/project_project.py
@@ -2,29 +2,9 @@ import logging
 
 from odoo import models
 
-from odoo.addons.base_display_name.tools import get_indexed_pattern
-
 _logger = logging.getLogger(__name__)
 
 
 class ProjectProject(models.Model):
     _inherit = "project.project"
 
-    def _compute_name_from_settings(self):
-        get_param = self.env["ir.config_parameter"].sudo().get_param
-        name_pattern = get_param("crm_sale_project.project_name_pattern")
-        if name_pattern:
-            field_paths = self._get_display_field_paths_from_pattern(name_pattern)
-            if field_paths:
-                indexed_pattern = get_indexed_pattern(name_pattern, field_paths)
-                name = self._get_value_from_indexed_pattern(
-                    self, field_paths, indexed_pattern
-                )
-                if name:
-                    self.name = name
-                else:
-                    _logger.warning(
-                        "Could not compute project name for project %s using pattern %s",
-                        self.id,
-                        name_pattern,
-                    )
--- partner_assign_location/models/res_city.py
+++ partner_assign_location/models/res_city.py
@@ -3,12 +3,6 @@ from odoo import api, fields, models
 
 class ResCity(models.Model):
     _inherit = "res.city"
-    # _inherit = ["res.city", "display.name.mixin"]
-
-    # @api.depends(lambda self: self._get_display_field_paths("display_name_pattern"))
-    # def _compute_display_name(self):
-    #     super()._compute_display_name()
-    #     self._set_field_from_pattern_fname("display_name", "display_name_pattern")
 
     def _compute_display_name(self):
         for r in self:
