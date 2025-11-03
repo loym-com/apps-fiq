ea0d9443906e328609e6984d690f246bb828e41a
Henrik Norlin <henrik.norl@gmail.com>
2025-11-03 21:05:53 +0100

    crm_name: disabled res_config_settings_views.xml

--- crm_name/__manifest__.py
+++ crm_name/__manifest__.py
@@ -8,7 +8,7 @@
     "author": "FIQ, Loym",
     "data": [
         "views/crm_lead_views.xml",
-        "views/res_config_settings_views.xml",
+        # "views/res_config_settings_views.xml",
     ],
     "depends": [
         "base_mixin_expression_value",
@@ -16,5 +16,5 @@
         "partner_short_name",
     ],
     "license": "AGPL-3",
-    "version": "18.0.5.1.11",
+    "version": "18.0.5.1.12",
 }
