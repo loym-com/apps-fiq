4400c57ef1a8743f02296bcb402653d1e22d59fd
Henrik Norlin <henrik.norl@gmail.com>
2025-11-12 01:30:19 +0100

    crm_sale_project: 18.0.5.1.13

--- crm_sale_project/__manifest__.py
+++ crm_sale_project/__manifest__.py
@@ -21,6 +21,6 @@
         "sale_timesheet", # to avoid: Invalid field 'billing_type' on model 'project.project'
     ],
     "license": "AGPL-3",
-    "version": "18.0.5.1.12",
+    "version": "18.0.5.1.13",
     "website": "https://www.loym.com",
 }
