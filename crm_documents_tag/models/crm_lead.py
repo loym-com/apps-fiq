5cad4c2b0c8fe6ca1bb0cc04078b25a4bca8c2b9
Henrik Norlin <henrik.norl@gmail.com>
2025-11-30 22:56:55 +0100

    [DEV] crm_documents_tag, partner_documents_tag

---
+++ crm_documents_tag/models/crm_lead.py
@@ -0,0 +1,12 @@
+from odoo import api, fields, models
+
+
+class CrmLead(models.Model):
+    _name = "crm.lead"
+    _inherit = ["crm.lead", "documents.tag.mixin"]
+
+    # def _get_document_vals(self, attachment):
+    #     vals = super()._get_document_vals(attachment)
+    #     if self.documents_tag_id:
+    #         vals['tag_id'] = self.documents_tag_id.id
+    #     return vals
---
+++ partner_documents_tag/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 Loym
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Contact Documents Tags",
+    "summary": "",
+    "author": "FIQ, Loym",
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "depends": [
+        "base",
+        "documents_tag",
+    ],
+    "license": "AGPL-3",
+    "version": "18.0.1.0.0",
+    "website": "https://www.loym.com",
+}
