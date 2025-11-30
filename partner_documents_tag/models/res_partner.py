c393c4dc697ccf0bffd4d53c0f577aa7ac0ae828
Henrik Norlin <henrik.norl@gmail.com>
2025-11-30 22:56:55 +0100

    [DEV] crm_documents_tag, partner_documents_tag

---
+++ partner_documents_tag/models/res_partner.py
@@ -0,0 +1,12 @@
+from odoo import api, fields, models
+
+
+class ResPartner(models.Model):
+    _name = "res.partner"
+    _inherit = ["res.partner", "documents.tag.mixin"]
+
+    # def _get_document_vals(self, attachment):
+    #     vals = super()._get_document_vals(attachment)
+    #     if self.documents_tag_id:
+    #         vals['tag_id'] = self.documents_tag_id.id
+    #     return vals
