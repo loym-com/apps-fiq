44c6392147d2447baf0efbda46f78ec4a9ef75e1
Henrik Norlin <henrik.norl@gmail.com>
2025-12-04 22:26:25 +0100

    5x_documents_tag: 18.0.x.x.x (except documents_tag)

--- partner_documents_tag/__manifest__.py
+++ partner_documents_tag/__manifest__.py
@@ -13,6 +13,6 @@
         "documents_tag",
     ],
     "license": "AGPL-3",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "website": "https://www.loym.com",
 }
--- project_documents_tag/__manifest__.py
+++ project_documents_tag/__manifest__.py
@@ -14,6 +14,6 @@
         "documents_tag",
     ],
     "license": "AGPL-3",
-    "version": "18.0.5.1.1",
+    "version": "18.0.5.1.2",
     "website": "https://www.loym.com",
 }
