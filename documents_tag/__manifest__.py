62582c890a277a5e06327711d51a5545a24ee17d
Henrik Norlin <henrik.norl@gmail.com>
2025-12-05 12:01:33 +0100

    documents_tag 18.0.5.2.2

--- documents_tag/__manifest__.py
+++ documents_tag/__manifest__.py
@@ -5,7 +5,7 @@
     "summary": "",
     "author": "FIQ, Loym",
     "website": "https://github.com/OCA/knowledge",
-    "version": "18.0.5.2.1",
+    "version": "18.0.5.2.2",
     "license": "AGPL-3",
     "depends": ["documents"],
     "data": [
