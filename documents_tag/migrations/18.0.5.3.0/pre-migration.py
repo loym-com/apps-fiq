04101dc96477446f0488b941f46b348c10368f93
Henrik Norlin <henrik.norl@gmail.com>
2025-12-07 03:17:34 +0100

    documents_tag: fix premigration (copied from 18)

--- documents_tag/migrations/18.0.5.3.0/pre-migration.py
+++ documents_tag/migrations/18.0.5.3.0/pre-migration.py
@@ -1,21 +1,12 @@
 from odoo import api, SUPERUSER_ID
 
 def migrate(cr, version):
-    """
-    Copy old fields into new mixin fields:
-    documents_tag_id  <- tag_id
-    documents_tag_tooltip_translate  <- tag_tooltip_translate
-    documents_tag_ids_filter  <- tag_ids_filter
-    """
 
-    # cr.execute("""
-    #     UPDATE documents_document
-    #     SET
-    #         documents_tag_id = tag_id,
-    #         documents_tag_tooltip_translate = tag_tooltip_translate,
-    #         documents_tag_ids_filter = tag_ids_filter
-    #     WHERE
-    #         tag_id IS NOT NULL
-    #         OR tag_tooltip_translate IS NOT NULL
-    #         OR tag_ids_filter IS NOT NULL;
-    # """)
+    cr.execute("""
+        ALTER TABLE documents_document
+        ADD COLUMN documents_tag_id integer;
+
+        UPDATE documents_document
+        SET documents_tag_id = tag_id
+        WHERE tag_id IS NOT NULL;
+    """)
