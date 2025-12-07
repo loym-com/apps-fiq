12ad28526173bbf288f5ba20535e53a4886ae2b2
Henrik Norlin <henrik.norl@gmail.com>
2025-12-07 03:09:24 +0100

    documents_tag: fix migraion third time

--- documents_tag/migrations/18.0.5.3.0/pre-migration.py
+++ documents_tag/migrations/18.0.5.3.0/pre-migration.py
@@ -3,9 +3,10 @@ from odoo import api, SUPERUSER_ID
 def migrate(cr, version):
 
     cr.execute("""
+        ALTER TABLE documents_document
+        ADD COLUMN documents_tag_id integer;
+
         UPDATE documents_document
-        SET
-            documents_tag_id = tag_id
-        WHERE
-            tag_id IS NOT NULL;
+        SET documents_tag_id = tag_id
+        WHERE tag_id IS NOT NULL;
     """)
