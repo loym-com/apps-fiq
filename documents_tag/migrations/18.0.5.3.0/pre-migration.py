2e22b4e5bc967a4e0ae6471fabb328b1343c83d9
Henrik Norlin <henrik.norl@gmail.com>
2025-12-07 03:05:49 +0100

    documents_tag: fix migraion again

--- documents_tag/migrations/18.0.5.3.0/pre-migration.py
+++ documents_tag/migrations/18.0.5.3.0/pre-migration.py
@@ -1,21 +1,11 @@
 from odoo import api, SUPERUSER_ID
 
 def migrate(cr, version):
-    """
-    Copy old fields into new mixin fields:
-    documents_tag_id  <- tag_id
-    documents_tag_tooltip_translate  <- tag_tooltip_translate
-    documents_tag_ids_filter  <- tag_ids_filter
-    """
 
     cr.execute("""
         UPDATE documents_document
         SET
-            documents_tag_id = tag_id,
-            documents_tag_tooltip_translate = tag_tooltip_translate,
-            documents_tag_color = tag_ids_color
+            documents_tag_id = tag_id
         WHERE
-            tag_id IS NOT NULL
-            OR tag_tooltip_translate IS NOT NULL
-            OR tag_color IS NOT NULL;
+            tag_id IS NOT NULL;
     """)
