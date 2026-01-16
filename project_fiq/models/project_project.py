af2095ac48a2151ef6e2b4067af7f2c7e2c112d3
Henrik Norlin <henrik@loym.com>
2026-01-16 16:57:55 +0100

    documents_code, project_fiq, fixes 2

--- project_fiq/models/project_project.py
+++ project_fiq/models/project_project.py
@@ -50,4 +50,4 @@ class ProjectProject(models.Model):
                         project.documents_folder_id.code = project.sequence_code
                 if not vals or "name" in vals:
                     project.documents_folder_id.name = project.name
-                    project.documents_folder_id.company_id = project.company_id
+                    project.documents_folder_id.company_id = project.company_id.id
