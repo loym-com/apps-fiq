31aeaa697b4bcb3848682a9a4fbcf257316585a8
Henrik Norlin <henrik@loym.com>
2026-01-26 20:12:53 +0100

    base_code_list, project_code_list: 18.0.1.0.x

--- base_code_list/__manifest__.py
+++ base_code_list/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Code Lists",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "category": "Tools",
     "license": "LGPL-3",
     "development_status": "Alpha",
--- project_code_list/__manifest__.py
+++ project_code_list/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Project Code Lists",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.0.2",
     "category": "Tools",
     "license": "LGPL-3",
     "development_status": "Production/Stable",
