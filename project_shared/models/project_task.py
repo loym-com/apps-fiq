93afb4d7b64b8b92583682542078010b54b9e3c1
Henrik Norlin <henrik@loym.com>
2026-02-03 11:42:13 +0100

    [ADD] project_shared

---
+++ project_shared/models/project_task.py
@@ -0,0 +1,23 @@
+from odoo import api, fields, models
+
+
+class ProjectTask(models.Model):
+    _inherit = "project.task"
+
+    shared_project = fields.Boolean(
+        related="project_id.shared",
+    )
+    shared = fields.Boolean(
+        string="Shared",
+        compute="_compute_shared",
+        store=True,
+        help="Indicates whether the task is shared (has a follower who is not a regular user)."
+    )
+
+    @api.depends("message_partner_ids")
+    def _compute_shared(self):
+        for task in self:
+            task.shared = any(
+                not partner.user_ids or partner.user_ids.filtered(lambda u: u.share)
+                for partner in task.message_partner_ids
+            )
