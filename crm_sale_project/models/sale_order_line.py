c3bba576986aa138e74788f22c199683bbca4bab
Henrik Norlin <henrik@loym.com>
2026-02-04 22:40:11 +0100

    crm_sale_project: add crm code-list-items to project/task

--- crm_sale_project/models/sale_order_line.py
+++ crm_sale_project/models/sale_order_line.py
@@ -6,12 +6,31 @@ from odoo.fields import Command
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    # def _timesheet_create_project(self):
-    #     # name
-    #     project = super()._timesheet_create_project()
-    #     if self.order_id.opportunity_id:
-    #         project.name = f"{self.order_id.name} {self.order_id.opportunity_id.name}"
-    #     return project
+    def _timesheet_get_project_name(self):
+        # return f"{self.order_id.name} {self.order_id.opportunity_id.name}"
+        return self.order_id.opportunity_id.name
+
+    def _timesheet_create_project(self):
+        project = super()._timesheet_create_project()
+        # name
+        if self.order_id.opportunity_id:
+            project.name = self._timesheet_get_project_name()
+        # crm_code_list, project_code_list
+        try:
+            usages = self.order_id.opportunity_id.code_list_usage_ids
+            if usages:
+                project.code_list_usage_ids = [
+                    (0, 0, {
+                        "model": "project.project",
+                        "res_id": project.id,
+                        "code_list_item_id": usage.code_list_item_id.id,
+                        "code_list_id": usage.code_list_id.id,
+                    })
+                    for usage in usages
+                ]
+        except AttributeError as e:
+            pass # no attr
+        return project
 
     def _timesheet_create_project_prepare_values(self):
         project_values = super()._timesheet_create_project_prepare_values()
@@ -22,6 +41,8 @@ class SaleOrderLine(models.Model):
         # Set no parent
         if "parent_id" in self.env["project.project"]._fields:
             project_values["parent_id"] = False
+        # sale_timesheet
+        project_values.setdefault('billing_type', 'not_billable')
         # Set assignments
         try: # assignment_ids (OCA/project project_role)
             template = self.product_id.project_template_id
@@ -56,16 +77,32 @@ class SaleOrderLine(models.Model):
         except AttributeError as e:
             pass # no attr
 
-        # sale_timesheet
-        project_values.setdefault('billing_type', 'not_billable')
-
         return project_values
 
+    def _timesheet_get_task_name(self):
+        partner = self.order_partner_id
+        return f"{self.product_id.name} - {partner.short_name or partner.name}"
+
     def _timesheet_create_task(self, project):
-        # Set name
         task = super()._timesheet_create_task(project)
+        # Set name
         if self.order_id.opportunity_id:
             task.name = self._timesheet_get_task_name()
+        # crm_code_list, project_code_list
+        try:
+            usages = self.order_id.opportunity_id.code_list_usage_ids
+            if usages:
+                task.code_list_usage_ids = [
+                    (0, 0, {
+                        "model": "project.task",
+                        "res_id": task.id,
+                        "code_list_item_id": usage.code_list_item_id.id,
+                        "code_list_id": usage.code_list_id.id,
+                    })
+                    for usage in usages
+                ]
+        except AttributeError as e:
+            pass # no attr
         return task
 
     def _timesheet_create_task_prepare_values(self, project):
@@ -76,7 +113,3 @@ class SaleOrderLine(models.Model):
         if "parent_id" in self.env["project.project"]._fields:
             task_values["parent_id"] = False
         return task_values
-
-    def _timesheet_get_task_name(self):
-        partner = self.order_partner_id
-        return f"{self.product_id.name} - {partner.short_name or partner.name}"
