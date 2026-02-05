9ee1c9947864cd1df500b0ee7b88140c68d582cd
Henrik Norlin <henrik@loym.com>
2026-02-05 08:43:03 +0100

    base_code_list: 1 list usage per record, documents_code_list: server actions

--- base_code_list/models/code_list_usage.py
+++ base_code_list/models/code_list_usage.py
@@ -4,11 +4,10 @@ class CodeListUsage(models.Model):
     _name = "code.list.usage"
     _description = "code.list.usage"
     _sql_constraints = [
-        # Consider code_list_uniq (only one usage of a code list per resource)
         (
-            "code_list_item_uniq",
-            "unique(model, res_id, code_list_item_id)",
-            "The code list item can be linked only once to the same resource!",
+            "code_list_uniq",
+            "unique(model, res_id, code_list_id)",
+            "The code list can be linked only once to the same resource!",
         ),
     ]
 
