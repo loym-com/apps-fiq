cc9564182562fd05fa4ada0da31fdccbdd8cdfc0
Henrik Norlin <henrik@loym.com>
2026-02-05 08:04:07 +0100

    3x code_list, crm_sale_project, documents_form: 18.0.x.x.x

--- documents_code_list/__manifest__.py
+++ documents_code_list/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Documents Code Lists",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "category": "Tools",
     "license": "LGPL-3",
     "development_status": "Alpha",
