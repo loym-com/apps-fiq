09cc542c1e19385b050bd1c3f96a8438fd9f21b6
Henrik Norlin <henrik@loym.com>
2026-05-07 16:39:29 +0200

    product_documents_tag: Command.link documents_tag_ids

--- product_documents_tag/models/product_product.py
+++ product_documents_tag/models/product_product.py
@@ -1,5 +1,7 @@
 from odoo import api, fields, models
 
+Command = fields.Command
+
 
 class ProductProduct(models.Model):
     _name = "product.product"
@@ -9,4 +11,8 @@ class ProductProduct(models.Model):
         vals = super()._get_document_vals(attachment)
         if self.documents_tag_id:
             vals["documents_tag_id"] = self.documents_tag_id.id
+        if self.documents_tag_ids:
+            vals["documents_tag_ids"] = [
+                Command.link(tag_id) for tag_id in self.documents_tag_ids.ids
+            ]
         return vals
