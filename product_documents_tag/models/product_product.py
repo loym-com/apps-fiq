97f5f77cd9ac30be6f4b572b31d1e81eb2192bf8
Henrik Norlin <henrik@loym.com>
2026-05-07 14:29:55 +0200

    product_documents_tag: _get_document_vals()

--- product_documents_tag/models/product_product.py
+++ product_documents_tag/models/product_product.py
@@ -5,8 +5,8 @@ class ProductProduct(models.Model):
     _name = "product.product"
     _inherit = ["product.product", "documents.tag.mixin"]
 
-    # def _get_document_vals(self, attachment):
-    #     vals = super()._get_document_vals(attachment)
-    #     if self.documents_tag_id:
-    #         vals["documents_tag_id"] = self.documents_tag_id.id
-    #     return vals
+    def _get_document_vals(self, attachment):
+        vals = super()._get_document_vals(attachment)
+        if self.documents_tag_id:
+            vals["documents_tag_id"] = self.documents_tag_id.id
+        return vals
