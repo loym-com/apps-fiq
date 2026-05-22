bd2775b05947295bfb02e02cf7cdc63de5ae9788
Henrik Norlin <henrik@loym.com>
2026-05-22 17:09:59 +0200

    base_code_list: User Tag (renamed because of name conflict)

--- base_code_list/models/res_users.py
+++ base_code_list/models/res_users.py
@@ -4,4 +4,5 @@ from odoo import models, fields
 class ResUsers(models.Model):
     _inherit = "res.users"
 
-    tag_ids = fields.Many2many("res.users.tag", string="Tags")
+    # "Tags" name conflicts with "category_id" field in "res.partner" model, so we use "User Tags" instead.
+    tag_ids = fields.Many2many("res.users.tag", string="User Tags")
